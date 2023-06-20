5681bf662bcc1dc1a924cffce187afd82ac5ba83
Manuel Vögele <develop@manuel-voegele.de>
2023-06-20 21:13:49 +0200

    Format with prettier

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import {libWrapper} from "./libwrapper_shim.js";
 
 Hooks.once("init", () => {
 	registerKeybindings();
-	hookFunctions()
-	window.addEventListener("mousemove", onMouseMove)
-})
+	hookFunctions();
+	window.addEventListener("mousemove", onMouseMove);
+});
 
 function registerKeybindings() {
 	game.keybindings.register("rangefinder", "activate", {
@@ -24,17 +24,19 @@ function hookFunctions() {
 }
 
 function getControlledToken() {
-	const controlled = canvas.tokens.controlled
+	const controlled = canvas.tokens.controlled;
 	switch (controlled.length) {
 		case 0:
 			// If no token is selected use the token of the users character
-			return canvas.tokens.placeables.find(token => token.actor.data._id === game.user.character?.data?._id)
+			return canvas.tokens.placeables.find(
+				token => token.actor.data._id === game.user.character?.data?._id,
+			);
 		case 1:
 			// If exactly one token is selected, take that
-			return controlled[0]
+			return controlled[0];
 		default:
 			// Do nothing if multiple tokens are selected
-			return undefined
+			return undefined;
 	}
 }
 
@@ -42,11 +44,9 @@ function activate(event) {
 	const ruler = canvas.controls.ruler;
 	// If the current ruler is a rangefinder don't refresh it
 	// This can happen because the browser may fire another event that doesn't have event.repeat set after a click on the canvas
-	if (ruler.isRangefinder)
-		return false;
-	const token = getControlledToken()
-	if (!token)
-		return false;
+	if (ruler.isRangefinder) return false;
+	const token = getControlledToken();
+	if (!token) return false;
 	ruler.clear();
 	ruler.isRangefinder = true;
 	ruler.rangefinderToken = token;
@@ -60,9 +60,8 @@ function activate(event) {
 function deactivate() {
 	const ruler = canvas.controls.ruler;
 	if (ruler.isRangefinder) {
-		ruler.isRangefinder = false
-		if (ruler._state !== Ruler.STATES.MOVING)
-			ruler._endMeasurement()
+		ruler.isRangefinder = false;
+		if (ruler._state !== Ruler.STATES.MOVING) ruler._endMeasurement();
 	}
 	return false;
 }
@@ -72,48 +71,45 @@ function onClickRight(wrapped, event) {
 		wrapped(event);
 	}
 	// Don't allow to remove the last waypoint from a rangefinder
-	else if ((this._state === 2) && (this.waypoints.length > 1)) {
-		this._removeWaypoint(event.data.origin, {snap: !event.data.originalEvent.shiftKey})
+	else if (this._state === 2 && this.waypoints.length > 1) {
+		this._removeWaypoint(event.data.origin, {snap: !event.data.originalEvent.shiftKey});
 	}
 }
 
 function onClickLeft(wrapped, event) {
 	const ruler = this.controls.ruler;
-	if (ruler.isRangefinder)
-		ruler._addWaypoint(event.data.origin);
-	else
-		wrapped(event);
+	if (ruler.isRangefinder) ruler._addWaypoint(event.data.origin);
+	else wrapped(event);
 }
 
 function onMouseUp(wrapped, event) {
-	if (!this.isRangefinder)
-		wrapped(event);
+	if (!this.isRangefinder) wrapped(event);
 }
 
 function onMouseMove(event) {
-	const ruler = canvas?.controls?.ruler
-	if (!ruler)
-		return
+	const ruler = canvas?.controls?.ruler;
+	if (!ruler) return;
 	if (ruler.isRangefinder) {
-		if (ruler._state === Ruler.STATES.MOVING)
-			return
-		measure(event)
+		if (ruler._state === Ruler.STATES.MOVING) return;
+		measure(event);
 	}
 }
 
 function measure(event) {
-	const ruler = canvas.controls.ruler
-	const canvasMousePos = canvas.app.renderer.plugins.interaction.mouse.getLocalPosition(canvas.tokens)
+	const ruler = canvas.controls.ruler;
+	const canvasMousePos = canvas.app.renderer.plugins.interaction.mouse.getLocalPosition(
+		canvas.tokens,
+	);
 
-	const mt = ruler.rangefinderMeasureTime ?? 0
+	const mt = ruler.rangefinderMeasureTime ?? 0;
 
 	// Hide any existing Token HUD
 	canvas.hud.token.clear();
 
 	// Draw measurement updates
-	if ( Date.now() - mt > 50 ) {
-	  ruler.measure(canvasMousePos, {gridSpaces: !event.shiftKey});
-	  ruler.rangefinderMeasureTime = Date.now();
-	  ruler._state = Ruler.STATES.MEASURING;
+	if (Date.now() - mt > 50) {
+		ruler.measure(canvasMousePos, {gridSpaces: !event.shiftKey});
+		ruler.rangefinderMeasureTime = Date.now();
+		ruler._state = Ruler.STATES.MEASURING;
 	}
 }
